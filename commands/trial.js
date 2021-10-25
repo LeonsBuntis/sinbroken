f54158753a7fc3bf344e007ccff3a86f14057474
patricnox <hello@patricnox.info>
2021-10-25 13:37:46 +0200

    Fix code style in trial.js

--- commands/trial.js
+++ commands/trial.js
@@ -11,38 +11,38 @@ module.exports = {
       if (!interaction.isCommand()) return;
       const { options, guild } = interaction;
 
-        if (!options) {
-          await interaction.reply('Something went wrong with the options');
-          return;
-        }
-        const name = options.getString('name');
-        const user = options.getUser('discordname');
-        const channelName = 'trial-' + name;
+      if (!options) {
+        await interaction.reply('Something went wrong with the options');
+        return;
+      }
+      
+      const name = options.getString('name');
+      const user = options.getUser('discordname');
+      const channelName = 'trial-' + name;
 
-        if (!user) return await interaction.reply("Please input a discord username.");
+      if (!user) return await interaction.reply("Please input a discord username.");
 
-        // create the new channel under category TRIALS ( config.trialChannelCategory )
-        await guild.channels.fetch(config.trialChannelCategory)
-          .then(category => {
-            // TODO: add trial to channel
-            category.createChannel(channelName, {
-              type: 'GUILD_TEXT',
-              topic: `Questions regarding ${name}'s trial.`
-            }).then(channel => {
-              channel.send(`Welcome to Sinbreakers ${name} (${user}). We have now created and marked you as a trial. Please make sure to read up on the raid-info channel.`);
-            });
-          }).catch(console.error);
+      // create the new channel under category TRIALS ( config.trialChannelCategory )
+      await guild.channels.fetch(config.trialChannelCategory).then(category => {
+          // TODO: add trial to channel
+          category.createChannel(channelName, {
+            type: 'GUILD_TEXT',
+            topic: `Questions regarding ${name}'s trial.`
+          }).then(channel => {
+            channel.send(`Welcome to Sinbreakers ${name} (${user}). We have now created and marked you as a trial. Please make sure to read up on the raid-info channel.`);
+          });
+      }).catch(console.error);
 
-        // TODO: implement the check for outside realm or not
-        // Add trial or trial (outside realm) based on what realm the trial is from.
-        const member = options.getMember('discordname');
-        await member.roles.add(config.trial_id);
+      // TODO: implement the check for outside realm or not
+      // Add trial or trial (outside realm) based on what realm the trial is from.
+      const member = options.getMember('discordname');
+      await member.roles.add(config.trial_id);
 
-        // TODO: Change user nickname to ingame name.
-        // Set nickname of the trial
-        await member.setNickname(name);
+      // TODO: Change user nickname to ingame name.
+      // Set nickname of the trial
+      await member.setNickname(name);
 
-        // Reply
-        await interaction.reply(`Created new trial channel for ${user}(${name})`);
+      // Reply
+      await interaction.reply(`Created new trial channel for ${user}(${name})`);
     }
 };
