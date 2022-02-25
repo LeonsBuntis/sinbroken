ef2873ed423ba8487a43fd336d76a4ecada22379
patricnox <patricnox@gmail.com>
2022-02-25 14:58:34 +0100

    implement trial vote

--- commands/trialvote.js
+++ commands/trialvote.js
@@ -1,10 +1,74 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const config = require("../config.json");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
-          .setName('trialvote')
-          .setDescription('Create a vote channel for the trial given by <name>')
-          .addStringOption(option => option.setName('name').setDescription('The ingame name of the trial')),
-    async execute(interaction) {
-        await interaction.reply('Not implemented');
+    .setName("trialvote")
+    .setDescription("Create a vote channel for the trial given by <name>")
+    .addStringOption((option) =>
+      option.setName("name").setDescription("The ingame name of the trial")
+    ),
+  async execute(interaction) {
+    if (!interaction.isCommand()) return;
+    const { options, guild } = interaction;
+
+    if (!options) {
+      await interaction.reply("Something went wrong with the options");
+      return;
     }
+
+    const name = options.getString("name");
+    const channelName = `vote-${name}`;
+
+    if (!name)
+      return await interaction.reply(
+        "Please input the players main character name."
+      );
+
+    // Create the new channel under category VOTE ( config.activeVotesCategory )
+    await guild.channels
+      .fetch(config.activeVotesCategory)
+      .then((category) => {
+        category
+          .createChannel(channelName, {
+            type: "GUILD_TEXT",
+            topic: `Vote for ${name}!`,
+          })
+          .then((channel) => {
+            channel.send(
+              `**VOTE: ${name}**\n\n:white_check_mark: - You see this trial as someone who fits our core team.\n\n:clock1: - You think we need some more time to see how it goes and how the performance changes.\n:x: - This trial is someone you don't think ever will fit us.\n\n`
+            );
+
+            let button1 = new MessageButton();
+            button1.setCustomId("trial-accept");
+            button1.setLabel("Accept");
+            button1.setStyle(3);
+
+            let button2 = new MessageButton();
+            button2.setCustomId("trial-extend");
+            button2.setLabel("Extend Trial");
+            button2.setStyle(2);
+
+            let button3 = new MessageButton();
+            button3.setCustomId("trial-reject");
+            button3.setLabel("Reject");
+            button3.setStyle(4);
+
+            const row = new MessageActionRow().addComponents([
+              button1,
+              button2,
+              button3,
+            ]);
+
+            channel.send({
+              components: [row],
+              content: "\nACCEPT:\nEXTEND:\nREJECT:\n",
+            });
+
+            // Reply
+            interaction.reply(`Started ${channel} for ${name}!`);
+          });
+      })
+      .catch(console.error);
+  },
 };
--- index.js
+++ index.js
@@ -1,25 +1,61 @@
 /*
-* Bootstrap the bot application.
-*/
-const { Client, Intents } = require('discord.js');
-const config = require('./config.json');
+ * Bootstrap the bot application.
+ */
+const { Client, Intents } = require("discord.js");
+const config = require("./config.json");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-const app = require('./core/app.js');
+const app = require("./core/app.js");
 
-client.once('ready', () => {
-	app.registerCommands(client);
+client.once("ready", () => {
+  app.registerCommands(client);
 });
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-    const command = client.commands.get(interaction.commandName);
-    if (!command) return;
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        if (error) console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    if (error) console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
+
+client.on("interactionCreate", async (button) => {
+  if (!button.isButton()) return;
+
+  let oldMsg = button.message.content;
+  console.log(oldMsg);
+  console.log(button.member.displayName);
+  if (button.message.content.includes(`${button.member.displayName}`)) {
+    oldMsg = button.message.content.replace(`${button.member.displayName}`, "");
+  }
+
+  switch (button.customId) {
+    case "trial-accept":
+      console.log("accept");
+      button.message.edit(
+        oldMsg.replace("ACCEPT:", `ACCEPT: ${button.member.displayName} `)
+      );
+      return button.deferUpdate();
+    case "trial-extend":
+      button.message.edit(
+        oldMsg.replace("EXTEND:", `EXTEND: ${button.member.displayName} `)
+      );
+      return button.deferUpdate();
+    case "trial-reject":
+      button.message.edit(
+        oldMsg.replace("REJECT:", `REJECT: ${button.member.displayName} `)
+      );
+      return button.deferUpdate();
+
+    default:
+      return button.deferUpdate();
+  }
 });
 
 client.login(config.token);
